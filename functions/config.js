dd0015755aa599b07c15013265db96eb7154086c
victorjambo <victorjambo@live.com>
2019-06-06 03:53:29 +0300

    gh-pages update

---
+++ functions/config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyBmEKmKa4d2m_aqya8nH_45FkdQqb7-KQI',
+  authDomain: 'personal-resume-f81c4.firebaseapp.com',
+  databaseURL: 'https://personal-resume-f81c4.firebaseio.com',
+  projectId: 'personal-resume-f81c4',
+  storageBucket: 'personal-resume-f81c4.appspot.com',
+  messagingSenderId: '1005730961132'
+  // appId: '1:916215437027:web:dab9af79f27286d9'
+};
