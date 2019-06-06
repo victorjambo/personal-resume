3dac8d05437956e7b9e8e0ab2bf461f56f002fa2
victorjambo <victorjambo@live.com>
2019-06-06 23:39:44 +0300

    scriptLoader

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
-import scriptLoader from 'react-async-script-loader';
 
 import Routes from './routes';
 
-const url = window.location.origin;
-const script = [
-  'https://www.googletagmanager.com/gtag/js?id=UA-136921529-1',
-  `${url}assets/plugins/js/jquery.min.js`,
-  `${url}assets/plugins/js/bootstrap.min.js`,
-  `${url}assets/plugins/js/wow.min.js`,
-  `${url}assets/plugins/js/circle-progress.js`,
-  `${url}assets/plugins/js/jquery.nav.js`,
-  `${url}assets/plugins/js/isotope.pkgd.js`,
-  `${url}assets/js/google-analytics.js`,
-  `${url}assets/js/custom-scripts.js`
-];
+// const url = window.location.origin;
+// const script = [
+//   'https://www.googletagmanager.com/gtag/js?id=UA-136921529-1',
+//   `${url}assets/plugins/js/jquery.min.js`,
+//   `${url}assets/plugins/js/bootstrap.min.js`,
+//   `${url}assets/plugins/js/wow.min.js`,
+//   `${url}assets/plugins/js/circle-progress.js`,
+//   `${url}assets/plugins/js/jquery.nav.js`,
+//   `${url}assets/plugins/js/isotope.pkgd.js`,
+//   `${url}assets/js/google-analytics.js`,
+//   `${url}assets/js/custom-scripts.js`
+// ];
 
 class App extends Component {
   render() {
@@ -22,4 +21,4 @@ class App extends Component {
   }
 }
 
-export default scriptLoader(script)(App);
+export default App;
